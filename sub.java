dc3c87720bc9e3134ba63a3affa11f85a8f25104
iamshaiksha <iamshaiksha2019@gmail.com>
2020-04-25 09:54:00 +0530

    subtract function available

--- sub.java
+++ sub.java
@@ -1,19 +1,16 @@
-<<<<<<< HEAD
+
 /**
  * 
  * @author shaiksha
  *
  */
-=======
-
->>>>>>> origin/master
 public class sub {
 
 	public static void main(String[] args) {
 		int a=20;
 		int b=20;
 		int c=a-b;
-		System.out.println(c);
+		System.out.println("c-->"+c);
 
 	}
 
