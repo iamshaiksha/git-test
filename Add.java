1b4e47bfb62a71404ce57904d0e017e9e372129f
iamshaiksha <iamshaiksha2019@gmail.com>
2020-04-25 10:30:46 +0530

    add function comment added

--- Add.java
+++ Add.java
@@ -2,7 +2,10 @@
 public class Add {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=20;
+		int b=20;
+		int c=a+b;
+		System.out.println("---->"+c);
 
 	}
 
